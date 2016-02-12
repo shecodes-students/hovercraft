2b31acc6ecf0458c929d594e7483f7f532a92613
Julia Friesel <julia.friesel@gmail.com>
2016-02-12 16:54:27 +0100

    add install script

---
+++ install.sh
@@ -0,0 +1,17 @@
+mkdir -p $HOME/hovercraft &&
+# download tar file and untar in ~/hovercraft
+# put .desktop file into autostart folder
+curl http://now.she.codes/hovercraft/hovercraft.tar.gz | tar -xz -C $HOME/hovercraft &&
+
+cat << EOF > $HOME/.config/autostart/hovercraft.desktop
+[Desktop Entry]
+Name[en]=Hovercraft
+Name[de]=Hovercraft
+Comment[en]=for Cordula 
+Comment[de]=für Cordula 
+Exec=$HOME/hovercraft/hovercraft-linux-x64/hovercraft\n
+Icon=application-default-icon
+X-GNOME-Autostart-enabled=true
+Type=Application
+EOF
+
