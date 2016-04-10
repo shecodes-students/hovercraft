9a8aadfc737204d2319b476fbb4f506152d183c9
Julia Friesel <julia.friesel@gmail.com>
2016-04-10 16:41:16 +0200

    fix indentation in clickerStream

--- clickerStream.js
+++ clickerStream.js
@@ -11,66 +11,66 @@ module.exports = (sentence) => {
     if (!sentence) return;
     const jitterWindow = conf.jitterWindow.curr;
     return pull(
-            pullMouse(electron.screen, 'clicker'),
-            // sliding-window de-jitter
-            pull.asyncMap( ( ()=> {
-                let values = [];
-                return (value, cb) => {
-                    values.unshift(value);
-                    if (values.length < jitterWindow) {
-                        return cb(null, undefined);
-                    }
-                    pull(
-                        pull.values(values),
-                        pull.reduce((a,b)=>{return {x:a.x+b.x, y:a.y+b.y};}, {x:0, y:0}, (err, sum)=> {
-                            values.pop();
-                            cb(err, {x: sum.x / jitterWindow, y: sum.y / jitterWindow});
-                        })
-                        );
-                };
-            })()),
-
-            // map position to distance
-            pull.map( (() => {
-                let previousPosition;
-                return (currentPosition)=> {
-                    if (previousPosition) {
-                        let distanceX = currentPosition.x - previousPosition.x;
-                        let distanceY = currentPosition.y - previousPosition.y;
-                        previousPosition = currentPosition;
-                        return Math.sqrt(Math.pow(distanceX,2) + Math.pow(distanceY,2));
-                    }
-                    previousPosition = currentPosition;
-                    return undefined;
-                };
-            })()),
-
-            // map distance to isResting
-            pull.map( (distance)=> {
-                if (typeof(distance) === 'undefined') {
-                    return undefined;
+        pullMouse(electron.screen, 'clicker'),
+        // sliding-window de-jitter
+        pull.asyncMap( ( ()=> {
+            let values = [];
+            return (value, cb) => {
+                values.unshift(value);
+                if (values.length < jitterWindow) {
+                    return cb(null, undefined);
                 }
-                return distance < conf.precisionThresholdPx.curr;
-            } ),
-            pullChanged(true),
-            pull.asyncMap(( () => { 
-                let timer = null;
-                let itsOver = false;
-                return (resting, cb) => {
-                    if (typeof(resting) === 'undefined') return cb(null, null);
-                    if (itsOver)  return cb(true);
-                    if (resting) {
-                        if (timer) clearTimeout(timer);
-                        timer = setTimeout( ()=>{
-                            itsOver = true;
-                            cb(null, sentence);
-                        }, conf.waitingTime.curr); 
-                    } else {
-                        clearTimeout(timer);
-                        cb(null, null);
-                    }
-                };
-            })()),
-            pull.filter( (x) => {return x !== null;} )
-                );
+                pull(
+                    pull.values(values),
+                    pull.reduce((a,b)=>{return {x:a.x+b.x, y:a.y+b.y};}, {x:0, y:0}, (err, sum)=> {
+                        values.pop();
+                        cb(err, {x: sum.x / jitterWindow, y: sum.y / jitterWindow});
+                    })
+                    );
+            };
+        })()),
+
+        // map position to distance
+        pull.map( (() => {
+            let previousPosition;
+            return (currentPosition)=> {
+                if (previousPosition) {
+                    let distanceX = currentPosition.x - previousPosition.x;
+                    let distanceY = currentPosition.y - previousPosition.y;
+                    previousPosition = currentPosition;
+                    return Math.sqrt(Math.pow(distanceX,2) + Math.pow(distanceY,2));
+                }
+                previousPosition = currentPosition;
+                return undefined;
+            };
+        })()),
+
+        // map distance to isResting
+        pull.map( (distance)=> {
+            if (typeof(distance) === 'undefined') {
+                return undefined;
+            }
+            return distance < conf.precisionThresholdPx.curr;
+        }),
+        pullChanged(true),
+        pull.asyncMap(( () => {
+            let timer = null;
+            let itsOver = false;
+            return (resting, cb) => {
+                if (typeof(resting) === 'undefined') return cb(null, null);
+                if (itsOver)  return cb(true);
+                if (resting) {
+                    if (timer) clearTimeout(timer);
+                    timer = setTimeout( ()=>{
+                        itsOver = true;
+                        cb(null, sentence);
+                    }, conf.waitingTime.curr);
+                } else {
+                    clearTimeout(timer);
+                    cb(null, null);
+                }
+            };
+        })()),
+        pull.filter( (x) => {return x !== null;} )
+    );
 };
